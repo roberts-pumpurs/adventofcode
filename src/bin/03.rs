b10c67d2ed288e910dd0c59c97a89ed69a766fa2
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2020-12-16 19:11:00 +0200

    Day 3: Part  2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -28,7 +28,7 @@ fn read_input() -> Vec<Vec<Terrain>> {
     contents
 }
 
-fn puzzle_part_1(data: &Vec<Vec<Terrain>>) -> u32 {
+fn puzzle_part(step_x: usize, step_y: usize, data: &Vec<Vec<Terrain>>) -> u32 {
     let mut tree_count = 0;
     let mut x = 0;
     let mut y = 0;
@@ -40,13 +40,13 @@ fn puzzle_part_1(data: &Vec<Vec<Terrain>>) -> u32 {
             Terrain::Tree => tree_count += 1,
             _ => (),
         }
-        y += 1;
-        if x + 3 > row.len() - 1 {
-            let delta: i32 = row.len() as i32 - (x + 3) as i32;
+        y += step_y;
+        if x + step_x > row.len() - 1 {
+            let delta: i32 = row.len() as i32 - (x + step_x) as i32;
             println!("delta {}", delta);
             x = delta.abs() as usize;
         } else {
-            x += 3;
+            x += step_x;
         }
     }
 
@@ -56,6 +56,11 @@ fn puzzle_part_1(data: &Vec<Vec<Terrain>>) -> u32 {
 fn main() {
     let input = read_input();
     time_it! {
-        (puzzle_part_1(&input), "")
+        let p1 = puzzle_part(3, 1, &input);
+        let p2_1 = puzzle_part(1, 1, &input);
+        let p2_3 = puzzle_part(5, 1, &input);
+        let p2_4 = puzzle_part(7, 1, &input);
+        let p2_5 = puzzle_part(1, 2, &input);
+        (p1, p1 * p2_1 * p2_3 * p2_4 * p2_5)
     };
 }
