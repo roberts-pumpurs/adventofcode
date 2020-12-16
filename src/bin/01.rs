c3e7435d07e83bac940fbdd3d04970852ec1deb8
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2020-12-16 08:22:51 +0200

    Day 1: part 2 finished

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::fs;
 
-use day_1::{time_it};
+use day_1::time_it;
 
 pub fn read_input() -> Vec<i32> {
     let filename = "src/bin/_01_input.txt";
@@ -14,7 +14,7 @@ pub fn read_input() -> Vec<i32> {
     contents
 }
 
-pub fn perform_task(mut input: Vec<i32>) -> Option<i32> {
+pub fn perform_task_one(mut input: Vec<i32>) -> Option<i32> {
     input.sort();
     for elem in input.iter() {
         for rev_elem in input.iter().rev() {
@@ -26,9 +26,27 @@ pub fn perform_task(mut input: Vec<i32>) -> Option<i32> {
     None
 }
 
+pub fn perform_task_two(mut input: Vec<i32>) -> Option<i32> {
+    input.sort();
+    for elem in input.iter() {
+        for rev_elem in input.iter().rev() {
+            let filtered: Vec<&i32> = input
+                .iter()
+                .filter(|e| *e != elem && *e != rev_elem)
+                .collect();
+            for middle in filtered {
+                if elem + rev_elem + middle == 2020 {
+                    return Some(elem * rev_elem * middle);
+                }
+            }
+        }
+    }
+    None
+}
+
 fn main() {
     let input = read_input();
     time_it! {
-        (perform_task(input).unwrap(), "")
+        (perform_task_one(input.clone()).unwrap(), perform_task_two(input.clone()).unwrap())
     };
 }
